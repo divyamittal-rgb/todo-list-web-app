2829678658892b751f2cc99add81f050e7eb992f
DivyaMittal <divyamittal1208@gmail.com>
2026-03-04 01:14:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,29 +3,35 @@ const listContainer = document.getElementById("list-container");
 const taskCounter = document.getElementById("taskCounter");
 
 function addTask() {
-    if (inputBox.value === '') {
+    if (inputBox.value.trim() === '') {
         alert("You must write something!");
-    } else {
-        let li = document.createElement("li");
-        li.innerText = inputBox.value;
-        listContainer.appendChild(li);
-
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
+        return;
     }
 
+    let li = document.createElement("li");
+    li.innerText = inputBox.value;
+    listContainer.appendChild(li);
+
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+
     inputBox.value = "";
+
     updateCounter();
+    saveData();
 }
 
 listContainer.addEventListener("click", function(e) {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         updateCounter();
-    } else if (e.target.tagName === "SPAN") {
+        saveData();
+    } 
+    else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         updateCounter();
+        saveData();
     }
 }, false);
 
@@ -35,7 +41,18 @@ function updateCounter() {
     let remaining = total - completed;
 
     taskCounter.innerText =
-        "Total " + total +
+        "Total: " + total +
         " | Completed: " + completed +
         " | Remaining: " + remaining;
 }
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data") || "";
+}
+
+showTask();
+updateCounter();
